6756a3ff0945a47358e3ee45f879eccc08ed401f
HanyeongOh <61238500+HanyeongOh@users.noreply.github.com>
2020-04-29 22:43:45 +0900

    drum complete!

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,7 +1,63 @@
-document
-  .querySelectorAll("button")
-  .forEach((element) => element.addEventListener("click", handleClick));
+document.querySelectorAll(".drum").forEach((element) =>
+  element.addEventListener("click", function () {
+    var key = this.innerHTML;
+    makeSound(key);
+    buttonAnimation(key);
+  })
+);
 
-function handleClick() {
-  alert("I got cliked!");
+document.addEventListener("keydown", function () {
+  var key = event.key;
+  makeSound(key);
+  buttonAnimation(key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    default:
+      console.log(event.key);
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+  document.querySelector("." + currentKey).classList.toggle("pressed");
+
+  setTimeout(function () {
+    document.querySelector("." + currentKey).classList.toggle("pressed");
+  }, 100);
 }
